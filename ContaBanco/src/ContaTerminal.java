c4ce910bbd7b4b9067ecea3663e7f26152fb06a2
guto1906dev <guto1906@yahoo.com.br>
2025-07-12 18:04:45 -0300

    desafio concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TO DO: Receber dados digitados
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        Integer conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        var agencia = scanner.next();
+
+        System.out.println("Por favor, digite o Nome do Cliente!");
+        var delimiterAtual = scanner.delimiter();
+        scanner.useDelimiter("\n");
+        String nomeCliente = scanner.next();
+        scanner.useDelimiter(delimiterAtual);
+
+
+        System.out.println("Por favor, digite o valor do Saldo!");
+        var saldo = Double.parseDouble(scanner.next());
+        
+
+        scanner.close();
+        //TO DO: Processar a mensgagem
+        var mensagem =  "Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo "+saldo+" já está disponível para saque.";
+        //TO DO: Exibir a resposta
+        System.out.println(mensagem);
     }
 }
